0cee1761ccfbf81c8dd69471938b8d488a9b0ac3
Lee Hyungi <hyungi.lee.622@gmail.com>
2022-07-02 17:01:37 +0900

    feat: Firehose IAM role terraform code 수정

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -85,103 +85,17 @@ policy = <<EOF
     "Version": "2012-10-17",
     "Statement": [
         {
-            "Sid": "",
             "Effect": "Allow",
-            "Action": [
-                "glue:GetTable",
-                "glue:GetTableVersion",
-                "glue:GetTableVersions"
-            ],
+            "Action": "s3:*",
             "Resource": [
-                "arn:aws:glue:ap-northeast-2:833496479373:catalog",
-                "arn:aws:glue:ap-northeast-2:833496479373:database/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%",
-                "arn:aws:glue:ap-northeast-2:833496479373:table/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
+                "${aws_s3_bucket.bucket.arn}",
+                "${aws_s3_bucket.bucket.arn}/*"
             ]
         },
         {
-            "Sid": "",
             "Effect": "Allow",
-            "Action": [
-                "s3:AbortMultipartUpload",
-                "s3:GetBucketLocation",
-                "s3:GetObject",
-                "s3:ListBucket",
-                "s3:ListBucketMultipartUploads",
-                "s3:PutObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::hg-project-stream-event-backup",
-                "arn:aws:s3:::hg-project-stream-event-backup/*"
-            ]
-        },
-        {
-            "Sid": "",
-            "Effect": "Allow",
-            "Action": [
-                "lambda:InvokeFunction",
-                "lambda:GetFunctionConfiguration"
-            ],
-            "Resource": "arn:aws:lambda:ap-northeast-2:833496479373:function:%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "kms:GenerateDataKey",
-                "kms:Decrypt"
-            ],
-            "Resource": [
-                "arn:aws:kms:ap-northeast-2:833496479373:key/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
-            ],
-            "Condition": {
-                "StringEquals": {
-                    "kms:ViaService": "s3.ap-northeast-2.amazonaws.com"
-                },
-                "StringLike": {
-                    "kms:EncryptionContext:aws:s3:arn": [
-                        "arn:aws:s3:::%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%/*",
-                        "arn:aws:s3:::%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
-                    ]
-                }
-            }
-        },
-        {
-            "Sid": "",
-            "Effect": "Allow",
-            "Action": [
-                "logs:PutLogEvents"
-            ],
-            "Resource": [
-                "arn:aws:logs:ap-northeast-2:833496479373:log-group:/aws/kinesisfirehose/KDS-S3-5tizk:log-stream:*",
-                "arn:aws:logs:ap-northeast-2:833496479373:log-group:%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%:log-stream:*"
-            ]
-        },
-        {
-            "Sid": "",
-            "Effect": "Allow",
-            "Action": [
-                "kinesis:DescribeStream",
-                "kinesis:GetShardIterator",
-                "kinesis:GetRecords",
-                "kinesis:ListShards"
-            ],
-            "Resource": "arn:aws:kinesis:ap-northeast-2:833496479373:stream/project-stream"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "kms:Decrypt"
-            ],
-            "Resource": [
-                "arn:aws:kms:ap-northeast-2:833496479373:key/%FIREHOSE_POLICY_TEMPLATE_PLACEHOLDER%"
-            ],
-            "Condition": {
-                "StringEquals": {
-                    "kms:ViaService": "kinesis.ap-northeast-2.amazonaws.com"
-                },
-                "StringLike": {
-                    "kms:EncryptionContext:aws:kinesis:arn": "arn:aws:kinesis:ap-northeast-2:833496479373:stream/project-stream"
-                }
-            }
+            "Action": "kinesis:*",
+            "Resource": "${aws_kinesis_stream.test_stream.arn}"
         },
         {
         "Effect": "Allow",
