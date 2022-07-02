3572cd0e8d5f16ab6d168ab96f5342049717a47e
Lee Hyungi <hyungi.lee.622@gmail.com>
2022-07-03 08:31:53 +0900

    feat: Zepplin에서 pySpark와 sql로 분석한 코드 업데이트

--- data-analysis/data-analysis.py
+++ data-analysis/data-analysis.py
@@ -29,3 +29,7 @@ ufo_sighting_df.show() # 상위 20개의 데이터를 출력
 ufo_sighting_df.createOrReplaceTempView("data_master")
 
 %sql
+# 한국에 대한 정보는 country column이 아닌 city에 국가명을 포함한 도시명이 표기가 되어있어 아래와 같은 조건으로 쿼리
+SELECT * FROM data_master WHERE city LIKE '%korea%'; # korea가 포함된 도시명 조건으로 검색하면 총 14개의 데이터가 있음을 확인
+
+# MapReduce로 comments에서 등장한 단어의 갯수 중에 가장 많이 등장한 단어를 분석한다. (조사 제외) 
